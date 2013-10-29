d8afe48d64b014e4683c077f3441d32b6d832f2e
Pavel Vasilyev <p_vasiliev@mail.ru>
2013-10-30 01:34:08 +0400

    committing latest changes in the App

--- src/main/java/com/griddynamics/spellcheck/main/MainApp.java
+++ src/main/java/com/griddynamics/spellcheck/main/MainApp.java
@@ -1,8 +1,5 @@
 package com.griddynamics.spellcheck.main;
 
-import com.griddynamics.spellcheck.core.NaiveSpellCheckEngine;
-
-import java.io.File;
 import java.io.IOException;
 
 /**
@@ -12,8 +9,6 @@ import java.io.IOException;
 public class MainApp {
 
     public static void main(String[] args) throws IOException {
-        final File file = new File("/Users/nepank/Documents/grid/pet-project-spellcheck/index");
-        final NaiveSpellCheckEngine engine = new NaiveSpellCheckEngine();
 
     }
 
